18a465145ba6a5bfb1c67164d7404472d972aa61
syncerpn <nghianteeek57@gmail.com>
2024-04-30 11:11:50 +0900

    Update 0001_two_sum.py

--- 0001_two_sum.py
+++ 0001_two_sum.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-#1. iterate through the list, add the number to a dict/set
-#2. check whether its counterpart is already in the dict/set
-def solve(nums: list, target: int) -> tuple:
+#1. iterate through the list, add the number to a dict
+#2. check whether its counterpart is already in the dict
+def solve(nums: list, target: int) -> list:
     nums_dict = {}
-    for n in nums:
+    for i, n in enumerate(nums):
         if target - n in nums_dict:
-            return (n, target-n)
-        nums_dict[n] = 1
+            return [i, nums_dict[target-n]]
+        nums_dict[n] = i
     
-    return ()
+    return []
