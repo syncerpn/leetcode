904e4b2af5b74cf13ff39338ba9ca1542d9feb8a
syncerpn <nghianteeek57@gmail.com>
2024-06-06 11:05:04 +0900

    ups

---
+++ 0846_1296_hand_of_straights.py
@@ -0,0 +1,20 @@
+#1. need to count first
+#2. as forming hands, recount the remaining cards
+def solve(hand: list, groupSize: int) -> bool:
+    hand_dict = {}
+    for i in hand:
+        if i not in hand_dict:
+            hand_dict[i] = 0
+        hand_dict[i] += 1
+    
+    for i in sorted(hand_dict.keys()):
+        n = hand_dict[i]
+        if n == 0:
+            continue
+        for j in range(1,groupSize):
+            if i+j not in hand_dict:
+                return False
+            if hand_dict[i+j] < n:
+                return False
+            hand_dict[i+j] -= hand_dict[i]        
+    return True
