21b65814ad34bcd6159d49e05a9729dfc5284073
syncerpn <nghianteeek57@gmail.com>
2024-08-20 10:30:48 +0900

    ups

---
+++ 1143_longest_common_subsequence.py
@@ -0,0 +1,16 @@
+# longest common dp problem that stays in my mind lol
+# can be optimized by storing only two rows instead of m-by-n dp
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        for i in range(m):
+            p = text1[i]
+            for j in range(n):
+                q = text2[j]
+                if p == q:
+                    dp[i+1][j+1] = dp[i][j] + 1
+                else:
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+        
+        return dp[-1][-1]
