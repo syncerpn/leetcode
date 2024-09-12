b26f5520b61f61dbfd7cdf4797a521cd1a14c5d5
syncerpn <nghianteeek57@gmail.com>
2024-09-12 16:47:51 +0900

    Update 0041_first_missing_positive.py

--- 0041_first_missing_positive.py
+++ 0041_first_missing_positive.py
@@ -27,3 +27,25 @@ class Solution:
             if nums[i] != i+1:
                 return i+1
         return i+2
+
+# O(1) space, O(n) time
+# brilliant point is to store additional information into the array
+# without losing existing information
+# just by % operator
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.append(0)
+        n = len(nums)
+        # delete those useless elements
+        for i in range(len(nums)):
+            if nums[i] < 0 or nums[i] >= n:
+                nums[i] = 0
+
+        # use the index as the hash to record the frequency of each number
+        for i in range(len(nums)):
+            nums[nums[i] % n] += n
+
+        for i in range(1,len(nums)):
+            if nums[i] // n == 0:
+                return i
+        return n
