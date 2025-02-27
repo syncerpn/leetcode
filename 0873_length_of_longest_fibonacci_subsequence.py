db3ebbccbcaf7792e8394a3679611d676ca8b25f
syncerpn <nghianteeek57@gmail.com>
2025-02-27 19:49:49 +0900

    ups

--- 0873_length_of_longest_fibonacci_subsequence.py
+++ 0873_length_of_longest_fibonacci_subsequence.py
@@ -1,6 +1,5 @@
-# kind of brute force like
-# properly can be further optimized
-# need to revisit
+# kind of brute force like, O(n2logn)
+# logn because fibo seq grows fast enough to be just logn
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         n = len(arr)
@@ -18,3 +17,16 @@ class Solution:
                 if k > 2:
                     ans = max(ans, k)
         return ans
+
+# dp O(n2)
+# dp[a, b] is length of the fibo seq ends up with (a, b)
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        dp = {}
+        s = set(arr)
+        for j in range(len(arr)):
+            for i in range(j):
+                ai, aj = arr[i], arr[j]
+                if aj - ai < ai and aj - ai in s:
+                    dp[ai, aj] = dp.get((aj - ai, ai), 2) + 1
+        return max(dp.values() or [0])
