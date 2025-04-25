6884e25b7ba7854773aab61d251ebe796128687b
syncerpn <nghianteeek57@gmail.com>
2025-04-25 19:10:04 +0900

    ups

---
+++ 2845_count_of_interesting_arrays.py
@@ -0,0 +1,12 @@
+# count of count
+# need revisited even though read it once
+class Solution:
+    def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
+        res = acc = 0
+        count = Counter({0: 1})
+        for a in nums:
+            acc = (acc + (1 if a % modulo == k else 0)) % modulo
+            res += count[(acc - k) % modulo]
+            count[acc] += 1
+        
+        return res
