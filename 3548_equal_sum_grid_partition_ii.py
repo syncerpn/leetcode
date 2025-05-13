fb3567421630901a94fec23bf3d9e8f2ea971d6e
syncerpn <nghianteeek57@gmail.com>
2025-05-13 22:50:03 +0900

    ups

--- 3548_equal_sum_grid_partition_ii.py
+++ 3548_equal_sum_grid_partition_ii.py
@@ -17,3 +17,26 @@ class Solution:
         if check(grid) or check(grid[::-1]): return True
         grid = list(zip(*grid))
         return check(grid) or check(grid[::-1])
+
+# still lee's solution, but i modified a bit
+# the first condition is kind of bugging me out
+# because one of the rect corners is missing from the check
+# shouldnt it be: "if top - bot in [0, A[0][0],  A[0][-1], A[i][0], A[i][-1]]: return True"
+# but the better is, you can remove A[0][-1]
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        def check(A):
+            seen = set()
+            top = 0
+            for i, r in enumerate(A):
+                seen |= set(r)
+                top += sum(r)
+                bot = total - top
+                if top - bot in [0, A[0][0], A[i][0]]: return True
+                if len(A[0]) > 1 and i > 0 and top - bot in seen: return True
+            return False
+        
+        total = sum(sum(r) for r in grid)
+        if check(grid) or check(grid[::-1]): return True
+        grid = list(zip(*grid))
+        return check(grid) or check(grid[::-1])
