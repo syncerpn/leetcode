6e7b5c6b26c2e1ef95e18efed59ef2e7a29f3ebf
syncerpn <nghianteeek57@gmail.com>
2025-12-24 13:23:36 +0900

    ups

--- 2054_two_best_non_overlapping_events.py
+++ 2054_two_best_non_overlapping_events.py
@@ -15,3 +15,16 @@ class Solution:
             else:
                 m = max(m, val)
         return ans
+
+# retry with mono stack and binary search
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        events.sort(key=lambda e: (e[1], e[0]))
+        h = [(0, 0)]
+        ans = 0
+        for s, e, v in events:
+            if not h or h[-1][1] < v:
+                h.append((e, v))
+            i = bisect.bisect(h, (s, 0))
+            ans = max(ans, h[i-1][1] + v)
+        return ans
